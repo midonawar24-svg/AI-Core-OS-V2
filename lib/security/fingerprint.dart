8db6b25ae104d676eac41aff146e7daf47e4b642
midonawar24-svg <midonawar24@gmail.com>
2026-08-01 06:49:48 +0300

    Add files via upload

---
+++ lib/security/fingerprint.dart
@@ -0,0 +1,13 @@
+import 'package:local_auth/local_auth.dart';
+
+class FingerprintService {
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  Future<bool> canCheck() async {
+    try { return await _auth.canCheckBiometrics && await _auth.isDeviceSupported(); } catch (_) { return false; }
+  }
+
+  Future<bool> auth() async {
+    try { return await _auth.authenticate(localizedReason: 'دخول AI Core OS V2', options: const AuthenticationOptions(biometricOnly: false, stickyAuth: true)); } catch (_) { return false; }
+  }
+}
