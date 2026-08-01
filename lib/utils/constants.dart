bdba273e9b413955b45e59c713993e7c21ada21f
midonawar24-svg <midonawar24@gmail.com>
2026-08-01 10:23:43 +0300

    Add files via upload

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String appName = 'AI Core OS V3';
+  static const String version = '3.0.0 - Neural Engine';
+  static const String description = 'Tokenizer + Embeddings + Transformer + Skills + DI';
+  static const double baseEvolution = 50.0;
+}
