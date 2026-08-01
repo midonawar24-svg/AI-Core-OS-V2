b36e7d7029b95970f590bb5f0dabc360f3820683
midonawar24-svg <midonawar24@gmail.com>
2026-08-01 06:51:58 +0300

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String appName = 'AI Core OS V2';
+  static const String version = '2.0.0';
+  static const String description = 'عقل مستقل 100% أوفلاين - بدون Gemini';
+  static const double baseEvolution = 50.0;
+  static const int maxMemory = 500;
+}
